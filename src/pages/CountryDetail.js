b07d90bdb366c34ffd0438abf6b229c61ef7df00
jfrye122 <jfryerome@gmail.com>
2023-02-07 13:30:27 -0500

    STARTED ADDING CALL EXAMPLE IN COUNTRYDETAILjs

--- src/pages/CountryDetail.js
+++ src/pages/CountryDetail.js
@@ -1,3 +1,10 @@
-export default function CountryDetail()  {
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+
+export default function CountryDetail({code})  {//JF: THIS is for asking a question about props
+    const { loading, error, data } = useQuery(
+
+    )
     return <h1>Country Detail List</h1>;
 };
