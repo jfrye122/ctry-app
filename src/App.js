5ac212b188eca500851f59e3dda1d214c6796faa
jfrye122 <jfryerome@gmail.com>
2023-02-23 16:14:43 -0500

    Moved code out of indexjs and into appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/HomePage/Home";
+import CountryDetail from "./pages/CountryDetail";
+import LanguageDetail from "./pages/LanguageDetail";
+import NoPage from "./pages/NoPage";
+import Layout from "./pages/Layout"; //temp for seeing pages
 
 function App() {
   return (
-   null
+    <BrowserRouter>
+      <Routes>
+        {
+          //PF: By having the nested route, the layout component will always render above the others.
+        }
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="country" element={<CountryDetail />} />
+          {
+            //PF Will need to add in routes that can support a path like `/country/:id`
+            //Make sure the component can accept the prop to figure out which id is being passed.
+          }
+          <Route path="country/:id" element={<CountryDetail />} />
+			    <Route path="language/:id" element={<CountryDetail />} />
+          <Route path="language" element={<LanguageDetail />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,54 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-//import App from './App';
+import App from './App';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import CountryDetail from "./pages/CountryDetail";
-import LanguageDetail from "./pages/LanguageDetail";
-import NoPage from "./pages/NoPage";
-import Layout from "./pages/Layout"; //temp for seeing pages
-
-import store from "./app/store";
-import { Provider } from "react-redux";
+//import store from "./app/store";
+//import { Provider } from "react-redux";
 
 //PF: Putting the apollo client into a utility class.
-
 //PF: Import as necessary to create the Apollo provider.
 import { ApolloProvider} from "@apollo/client";
 
 //init GraphQL client
 import { client } from './util/utils'
-
-//const LIST_CCONTINENTS = client.query({query : gql`})
-
-
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {
-          //PF: By having the nested route, the layout component will always render above the others.
-        }
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="country" element={<CountryDetail />} />
-          {
-            //PF Will need to add in routes that can support a path like `/country/:id`
-            //Make sure the component can accept the prop to figure out which id is being passed.
-          }
-          <Route path="country/:id" element={<CountryDetail />} />
-			    <Route path="language/:id" element={<CountryDetail />} />
-          <Route path="language" element={<LanguageDetail />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
