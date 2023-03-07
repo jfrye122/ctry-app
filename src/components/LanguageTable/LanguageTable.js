353c9b8c4c5bc3d5eebac71c6c90a218d916a43d
jfrye122 <jfryerome@gmail.com>
2023-03-07 17:13:08 -0500

    Update LanguageTable to use Card List from antd

--- src/components/LanguageTable/LanguageTable.js
+++ src/components/LanguageTable/LanguageTable.js
@@ -1,8 +1,7 @@
-import LanguageRow from "../LanguageRow/LanguageRow";
-
 import { useQuery } from "@apollo/client";
 import { GET_LANGUAGES } from "../../util/utils";
 import { Link } from "react-router-dom";
+import { Card, List } from "antd";
 
 export default function LanguageTable() {
   const { loading, error, data } = useQuery(GET_LANGUAGES);
@@ -12,27 +11,19 @@ export default function LanguageTable() {
   if (error) {
     return <div>Error: {JSON.stringify(error, null, 3)}</div>;
   }
-
-  const tableItems = data.languages.map((language) => (
-    <tr>
-      <td colSpan="2" align="center">
-        <Link to={`../language/${language.code}`} key={language.code}>
-          {language.name}
-        </Link>
-      </td>
-    </tr>
-    // <LanguageRow name={language.name} key={language.code} />
-    //<li key={language.code}>{language.name}</li>
-  ));
-
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Languages</th>
-        </tr>
-      </thead>
-      <tbody>{tableItems}</tbody>
-    </table>
+    <Card title="Languages" type="inner">
+      <List
+        // bordered
+        dataSource={data.languages}
+        renderItem={(item) => (
+          <List.Item>
+            <Link to={`../language/${item.code}`} key={item.code}>
+              {item.name}
+            </Link>
+          </List.Item>
+        )}
+      />
+    </Card>
   );
 }
