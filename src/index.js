cf8bdbb163ff386d856a679c6719619cda5415bc
jfrye122 <jfryerome@gmail.com>
2023-03-07 17:11:17 -0500

    Added ConfigProvider to index.js for themes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from './App';
+import App from "./App";
 
 //import store from "./app/store";
 //import { Provider } from "react-redux";
 
 //PF: Putting the apollo client into a utility class.
 //PF: Import as necessary to create the Apollo provider.
-import { ApolloProvider} from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 
 //init GraphQL client
-import { client } from './util/utils'
+import { client } from "./util/utils";
+import { ConfigProvider, theme} from "antd";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-
+    <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
       <App />
-
+    </ConfigProvider>
   </ApolloProvider>
 );
